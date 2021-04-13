f1ce78754bd4b01dc2ef8022b6317b40288579f6
davidasrobot <yogasakajr@gmail.com>
2021-04-13 05:20:34 +0000

    from server

--- server/settings.py
+++ server/settings.py
@@ -80,15 +80,11 @@ DATABASES = {
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        }
     }
 }
 
